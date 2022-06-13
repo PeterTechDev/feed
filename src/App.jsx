8cd63a0f7efc62587b30f3d99f679d3db6b136ad
unknown <peterleite.dev@gmail.com>
2022-06-13 06:06:13 -0300

    starting sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
 
 import './global.css'
+
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
       <Header />
-        <h1>Hello Peter</h1>
-      <Post />
+
+      <div className={styles.wrapper}>
+      <Sidebar />
+
+        <main>
+          <h1>Aqui quem fala é a main</h1>
+        </main>
+
+        <Post />
+      </div>
     </>
   )
 }
